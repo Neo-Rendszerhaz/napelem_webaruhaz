cb9c8df8d9e6424eb4fdd3909fc4acea576e81a3
Bláj3n <ulrichbence15@freemail.hu>
2023-02-09 14:34:42 +0100

    kezdőlap termékek megjelenítése kártyákon

--- public/js/view/kezdolapView/KezdolapView.js
+++ public/js/view/kezdolapView/KezdolapView.js
@@ -3,10 +3,11 @@ class KezdolapView {
     constructor(elem, szuloElem) {
         this.#elem = elem;
         szuloElem.append(`
-            <div>
-                <h1>Név: ${elem.megnevezes}</h1>
+            <div class="termek">
+                <h2>Név: ${elem.megnevezes}</h2>
                 <img src="${elem.kep}">
                 <h3>Ár: ${elem.ar}</h3>
+                <button id="gomb${elem.id}">Kosárba</button>
             </div>
             `)
     }
--- public/js/view/kezdolapView/KezdolapokView.js
+++ public/js/view/kezdolapView/KezdolapokView.js
@@ -3,16 +3,9 @@ import KezdolapView from "./KezdolapView.js";
 class KezdoLapokView {
     constructor(tomb, szuloElem) {
         //console.log(tomb.termekek);
-        szuloElem.html(
-            `<div>
-                <h1></h1>
-                <p><a></p>
-                <img>
-                <h3></h3>
-            </div>`)
         this.divElem = szuloElem.children("div:last-child");
         tomb.termekek.forEach(adat => {
-            const adatom = new KezdolapView(adat, this.divElem);
+            const adatom = new KezdolapView(adat, szuloElem);
         });
 
     }
