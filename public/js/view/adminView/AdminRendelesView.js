0c1573c400cb72214b0ec898fb97e1fce4a348da
Xu Jiyu <matexu97@gmail.com>
2023-02-09 12:39:34 +0100

    rendeles adatok megjelenítése

--- public/js/view/adminView/AdminRendelesView.js
+++ public/js/view/adminView/AdminRendelesView.js
@@ -1,6 +1,18 @@
-class AdminRendelesView{
-    constructor(){
-
-    }
+class AdminRendelesView {
+  constructor(obj, szuloElem) {
+    $(szuloElem).append(`
+        <tr>
+        <td>${obj.rendeles_szam}</td>
+        <td>${obj.datum}</td>
+        <td>${obj.vezeteknev} ${obj.keresztnev}</td>
+        <td>${obj.szallitasi_cim}</td>
+        <td>${obj.vegosszeg}</td>
+        <td>${obj.kedvezmeny}</td>
+        <td>${obj.kedvezmenyes_ar}</td>
+        <td>${obj.allapot}</td>
+        <td><button>Megjelenítés</button></td>
+        <td><button>Modosítás</button></td>
+        </tr>`)
+  }
 }
 export default AdminRendelesView
