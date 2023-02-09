6ac18a5c8769421c2cbcfa947be20006320c52fe
Bláj3n <ulrichbence15@freemail.hu>
2023-02-09 13:16:56 +0100

    index.php Admin Felület menüpont

--- resources/views/oldalak/index.php
+++ resources/views/oldalak/index.php
@@ -1,30 +1,34 @@
 <!DOCTYPE html>
 <html lang="hu-HU">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.1.js"
+        integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
     <script src="/../js/index.js" type="module"></script>
     <title>Webáruház</title>
 </head>
+
 <body>
     <main>
         <header>
             <nav>
                 <ul>
                     <li><a href="/profil">Profil</a></li>
-                    <li><a href="/r_tetelek">Rendelés tételek</a></li>
+                    <li><a href="/admin_felulet">Admin Felület</a></li>
                     <li><a href="/">Kezdőlap</a></li>
                 </ul>
             </nav>
         </header>
         <article>
-            
+
         </article>
         <footer>
-            
+
         </footer>
     </main>
 </body>
+
 </html>
