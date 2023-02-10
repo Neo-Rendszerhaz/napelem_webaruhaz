9175f275827e331d7fa2f4f60383022c8df4b21a
Bláj3n <ulrichbence15@freemail.hu>
2023-02-10 15:26:54 +0100

    Admin oldal Haszon  gomb működése és adatok megjelenítése táblában

--- public/js/view/kezdolapView/KezdolapView.js
+++ public/js/view/kezdolapView/KezdolapView.js
@@ -6,7 +6,7 @@ class KezdolapView {
             <div class="termek">
                 <h2>Név: ${elem.megnevezes}</h2>
                 <img src="${elem.kep}">
-                <h3>Ár: ${elem.ar}</h3>
+                <h3>Ár: ${elem.ar} Ft</h3>
                 <button id="gomb${elem.id}">Kosárba</button>
             </div>
             `)
