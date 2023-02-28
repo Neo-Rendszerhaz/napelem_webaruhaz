0b6759c65604e0179cb4a8a7b25d7e60b9231452
Xu Jiyu <matexu97@gmail.com>
2023-02-28 10:59:23 +0100

    rendelés szűrő

--- public/js/controller/adminController/AdminRendelesTetelController.js
+++ public/js/controller/adminController/AdminRendelesTetelController.js
@@ -8,15 +8,34 @@ class AdminRendTetelC{
         const vegpont = "/rendeles_tetelek_termekkel";
         AFM.adatBe(vegpont,this.tetelMutat)
         $(window).on("RTtorles",(event)=>{
-            const torlesPont = `/rendeles_tetelek/${event.detail.rendeles_szam}/${event.detail.termek_id}`
-            AFM.adatTorol(torlesPont,event.detail)
-            AFM.adatBe(vegpont,this.tetelMutat)
+            let figyelmeztetes = confirm("Biztosan szeretné törölni?");
+            if(figyelmeztetes === true){         
+                const torlesPont = `/rendeles_tetelek/${event.detail.rendeles_szam}/${event.detail.termek_id}`
+                AFM.adatTorol(torlesPont,event.detail)
+                AFM.adatBe(vegpont,this.tetelMutat)
+            }
         })
     }
     tetelMutat(tomb){
         // const szuloElem = "article"
         const szuloElem = "#adatTarolo"
         new AdminRendTetelekV(tomb, szuloElem);
+    }
+    myFunction() {
+        var input, filter, ul, li, a, i, txtValue;
+        input = document.getElementById("myInput");
+        filter = input.value.toUpperCase();
+        ul = document.getElementById("myUL");
+        li = ul.getElementsByTagName("li");
+        for (i = 0; i < li.length; i++) {
+            a = li[i].getElementsByTagName("a")[0];
+            txtValue = a.textContent || a.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                li[i].style.display = "";
+            } else {
+                li[i].style.display = "none";
+            }
+        }
     }
 }
 export default AdminRendTetelC
