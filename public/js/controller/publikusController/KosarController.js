c49b7f98d14f99dbc514b9d0e0a2e6b81adbcdaa
WeinbergerPeti <weinbergerpeti@gmail.com>
2023-03-23 12:56:25 +0100

    rendelés véglegesítés gomb kattint kosár kiiratása konzolra - Bence

--- public/js/controller/publikusController/KosarController.js
+++ public/js/controller/publikusController/KosarController.js
@@ -1,10 +1,16 @@
+import AdatFeldolgozModel from "../../model/AdatFeldolgozModel.js";
 import KosarakView from "../../view/publikusView/KosarakView.js";
+
 
 class KosarController 
 {
+    #rendelesAdatok={};
     constructor() 
     {        
         this.kosar=[];
+        this.#rendelesAdatok={megnevezes:"", cikkszam:"", gyartoi_cikkszam:"", marka:"", garancia:"", leiras:""};
+        const token = $(`meta[name="csrf-token"]`).attr("content");
+        const adatFeldolgozModel = new AdatFeldolgozModel(token);
 
         let jsonObjektum = JSON.parse(localStorage.getItem("kosar"));
         for (let i = 0; i < jsonObjektum.length; i++) 
@@ -13,6 +19,14 @@ class KosarController
         }
 
         this.kosarAdatok(this.kosar)
+        $(window).on("rendelesVeglegesites", ()=>
+        {
+            console.log(this.kosar);
+            adatFeldolgozModel.adatUj("/termekek", this.kosar);
+
+        });
+        this.rendelesMent();
+        console.log(this.#rendelesAdatok);
     }
 
     kosarAdatok(tomb)
@@ -21,5 +35,14 @@ class KosarController
         new KosarakView(tomb, szuloelem);
     }
 
+    rendelesMent()
+    {
+        this.#rendelesAdatok.megnevezes=$("#megnevezes").val();
+        this.#rendelesAdatok.cikkszam=$("#cikkszam").val();
+        this.#rendelesAdatok.gyartoi_cikkszam=$("#gyartoi_cikkszam").val();
+        this.#rendelesAdatok.marka=$("#marka").val();
+        this.#rendelesAdatok.garancia=$("#garancia").val();
+        this.#rendelesAdatok.leiras=$("#leiras").val();
+    }
 }
 export default KosarController;
