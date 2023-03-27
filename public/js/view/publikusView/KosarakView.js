fe955571cae7c7ebc4a0c38e45f28c9e77bb2887
Bláj3n <ulrichbence15@freemail.hu>
2023-03-27 12:41:53 +0200

    számlázási és szállítási cím megjelenítés/eltüntetés

--- public/js/view/publikusView/KosarakView.js
+++ public/js/view/publikusView/KosarakView.js
@@ -109,17 +109,112 @@ class KosarakView {
                         </div>
                         <div class="row mt-3">
                             <div class="col-3">
-                                <input id="azonosCim" class="w-full" type="checkbox" name="azonosCim">
+                                <input id="azonosCim" class="w-full" type="checkbox" name="azonosCim" checked="checked">
                             </div>
                             <div class="col-md">
-                                <label for="azonosCim" class="col-form-label">Számlázási és szállítási cím megegyezik</label><br>
+                                <label for="azonosCim" class="col-form-label">Számlázási és szállítási cím NEM egyezik meg</label><br>
                             </div>
                         </div>
                         </div>
-                        <input type="button" id="rendelesVeglegesites" class="btn btn-primary mt-2"  value="Rendelés véglegesítése">
+                        
                     </form>
-                    
-                </div>
+                    <form id="szallitasiCim">
+                    <h3>Szállítási cím megadása</h3>
+                    <div id="formTartalom" class="form-group">
+                        <div class="row mt-3">
+                            <div class="col-3">
+                                <label for="iranyitoszam" class=" col-form-label">Irányítószám</label>
+                            </div>
+                            <div class="col-md">
+                                <input id="iranyitoszam" class="w-full" type="text" name="iranyitoszam" placeholder="1234" required ="iranyitoszam" />
+                            </div>
+                        </div>
+        
+                        <div class="row mt-3">
+                            <div class="col-3">
+                                <label for="varos" class=" col-form-label">Város</label>
+                            </div>
+                            <div class="col-md">
+                                <input id="varos" class="w-full" type="text" name="varos" placeholder="Város" required ="varos" />
+                            </div>
+                        </div>
+        
+                        <div class="row mt-3">
+                            <div class="col-3">
+                                <label for="kozteruletNeve" class=" col-form-label">Közterület neve</label>
+                            </div>
+                            <div class="col-md">
+                                <input id="kozteruletNeve" class="w-full" type="text" name="kozteruletNeve" placeholder="Közterület neve" required ="kozteruletNeve" />
+                            </div>
+                        </div>
+        
+                        <div class="row mt-3">
+                            <div class="col-3">
+                                <label for="kozteruletJellege" class=" col-form-label">Közterület jellege</label>
+                            </div>
+                            <div class="col-md">
+                                <input id="kozteruletJellege" class="w-full" type="text" name="kozteruletJellege" placeholder="Közterület jellege" required ="kozteruletJellege" />
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class=" form-check form-check-inline">
+                                <input id="hazJelleg" class="form-check-input" type="radio" name="helyHazJelleg" value="hsz" checked>
+                                <label for="hazJelleg" class="form-check-label">Házszám</label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input id="helyrajzJelleg" class="form-check-input" type="radio" name="helyHazJelleg" value="hrsz">
+                                <label for="helyrajzJelleg" class="form-check-label">Helyrajzi szám</label>
+                            </div>
+                        </div>
+
+                        <div class="row mt-3">
+                            <div class="col-3">
+                                <label for="helyHazSzam" class=" col-form-label">Ház / helyrajzi szám</label>
+                            </div>
+                            <div class="col-md">
+                                <input id="helyHazSzam" class="w-full" type="text" name="helyHazSzam" placeholder="Ház / helyrajzi szám" required ="helyHazSzam" />
+                            </div>
+                        </div>
+        
+                        <div class="row mt-3">
+                            <div class="col-3">
+                                <label for="epulet" class=" col-form-label">Épület</label>
+                            </div>
+                            <div class="col-md">
+                                <input id="epulet" class="w-full" type="text" name="epulet" placeholder="Épület" required ="epulet" />
+                            </div>
+                        </div>
+        
+                        <div class="row mt-3">
+                            <div class="col-3">
+                                <label for="emelet" class=" col-form-label">Emelet</label>
+                            </div>
+                            <div class="col-md">
+                                <input id="emelet" class="w-full" type="text" name="emelet" placeholder="Emelet" required ="emelet" />
+                            </div>
+                        </div>
+        
+                        <div class="row mt-3">
+                            <div class="col-3">
+                                <label for="ajto" class=" col-form-label">Ajtó szám</label>
+                            </div>
+                            <div class="col-md">
+                                <input id="ajto" class="w-full" type="text" name="ajto" placeholder="Ajtó szám" required ="ajto" />
+                            </div>
+                        </div>
+        
+                        <div class="row mt-3">
+                            <div class="col-3">
+                                <label for="kapucsengo" class="col-form-label">Kapucsengő</label>
+                            </div>
+                            <div class="col-md">
+                                <input id="kapucsengo" class="w-full" type="text" name="kapucsengo" placeholder="Kapucsengő" required ="kapucsengo" />
+                            </div>
+                        </div>
+                    </form>
+                    </div>
+                    <input type="button" id="rendelesVeglegesites" class="btn btn-primary mt-2"  value="Rendelés véglegesítése">
             </div>
         </div>`)
 
@@ -152,6 +247,15 @@ class KosarakView {
             console.log("bezár");
             $(".overlay").hide();
         });
+
+        $(`#azonosCim`).on("click", () => {
+            let bePipalva = $(`#azonosCim`).prop('checked');
+            if (bePipalva == true) {
+                $("#szallitasiCim").show();
+            } else {
+                $("#szallitasiCim").hide();
+            }
+        })
     }
 
     objektbe(tomb) {
@@ -189,12 +293,12 @@ class KosarakView {
         //     } else {
         //         this.#rendelesCimAdatok.kapucsengo = $("#kapucsengo").val();
         //     }
-        }
-
-        kattintasTrigger(esemenyNeve) {
-            console.log("triggerben", esemenyNeve);
-            const esemeny = new CustomEvent(esemenyNeve, { detail: {/* "termekek":this.tombMentes ,*/ "cim": this.#rendelesCimAdatok } });
-            window.dispatchEvent(esemeny);
-        }
     }
+
+    kattintasTrigger(esemenyNeve) {
+        console.log("triggerben", esemenyNeve);
+        const esemeny = new CustomEvent(esemenyNeve, { detail: {/* "termekek":this.tombMentes ,*/ "cim": this.#rendelesCimAdatok } });
+        window.dispatchEvent(esemeny);
+    }
+}
 export default KosarakView;
